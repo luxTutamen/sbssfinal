a7ff1271411258a7a25922ccd977009703111af4
kvzmdssos@gmail.com <kovez@protonmail.com>
2020-05-09 11:36:31 +0300

    javadoc added

--- src/main/java/ua/axiom/controller/MultiViewController.java
+++ src/main/java/ua/axiom/controller/MultiViewController.java
@@ -10,18 +10,35 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+/**
+ * Provides abstraction of a ViewController, that has different View and Controllers, depending on
+ */
 public abstract class MultiViewController {
     private final Map<Supplier<Boolean>, Function<Model, ModelAndView>> requestMapping = new HashMap<>();
 
+    /**
+     * Adds new controller
+     * @param useCase controller is used, if this is true
+     * @param controller the controller itself, consumes Model, so chains of an controllers can be used
+     */
     protected void addController(Supplier<Boolean> useCase,  Function<Model, ModelAndView> controller) {
         requestMapping.put(useCase, controller);
     }
 
+    /**
+     * Consumes model, and returns ModelAndView with data from Model and also data, provided by controller
+     * @param model with data from previous controller
+     * @return ModelAndView with data and pattern template
+     */
     public ModelAndView getRequestMapping(Model model) {
         return getViewController()
                 .apply(model);
     }
 
+    /**
+     * Gets one controller from "requestMapping", based on predicate
+     * @return the only Controller, that fits the predicate requirements
+     */
     private Function<Model, ModelAndView> getViewController() {
         List<Function<Model, ModelAndView>> mapping = requestMapping
                 .entrySet()
--- src/main/java/ua/axiom/controller/MustacheController.java
+++ src/main/java/ua/axiom/controller/MustacheController.java
@@ -8,20 +8,49 @@ import ua.axiom.model.objects.UserLocale;
 import java.util.Map;
 
 /**
- *
+ * Abstract representation of a mustache VC, contains major events in a dynamic webpage creation
+ * Only works with logged in users
  * @param <T>
  */
 public abstract class MustacheController <T extends User> {
-    protected abstract void fillUserSpecificData(Map<String, Object> model, T user);
-
-    protected abstract void fillLocalisedPageData(Map<String, Object> model, UserLocale locale);
-
-    protected abstract T getPersistedUser();
-
+    /**
+     * Return new ModelAndView from here with needed template file and given model
+     * @param model constructed model, populated with localised GUI text, and user data
+     * @return ModelAndView to go for
+     */
     protected abstract ModelAndView formResponse(Map<String, Object> model);
 
+    /**
+     * Incapsulates specific on-request action (like check for bonuses, request count, etc)
+     * @param user that is requesting the page
+     */
     public abstract void processRequest(T user);
 
+    /**
+     * Gets current logged id from a context and uses it to get user from a database
+     * @return USer object from a database
+     */
+    protected abstract T getPersistedUser();
+
+    /**
+     * Populates model with data, that needs USer object to be calculated (like locale, username, etc)
+     * @param model model to put data into
+     * @param user
+     */
+    protected abstract void fillUserSpecificData(Map<String, Object> model, T user);
+
+    /**
+     * Populates model with template-specific, localised date (button text, etc)
+     * @param model model to put data into
+     * @param locale
+     */
+    protected abstract void fillLocalisedPageData(Map<String, Object> model, UserLocale locale);
+
+    /**
+     * processes request.  Containing class must be annotated with @RequestMapping with specific URI!
+     * @param model to put data into
+     * @return ModelAndView with model, filled with localised data, and View as a specific Mustache template path
+     */
     @RequestMapping
     public final ModelAndView serveRequest(Map<String, Object> model) {
         T user = getPersistedUser();
